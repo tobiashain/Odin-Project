a6820ed400c3db83c4abd076a663c709b8fe501a
Tobinator647 <hain.tobias@outlook.at>
2025-11-03 17:22:21 +0100

    feat: Library 3D finish

--- Library/library.ts
+++ Library/library.ts
@@ -28,10 +28,60 @@ interface AddBookForm {
 
 const libraryMap = new Map();
 
+const books = [
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 281 },
+  { title: '1984', author: 'George Orwell', pages: 328 },
+  { title: 'Moby-Dick', author: 'Herman Melville', pages: 635 },
+  { title: 'Pride and Prejudice', author: 'Jane Austen', pages: 279 },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', pages: 180 },
+  { title: 'War and Peace', author: 'Leo Tolstoy', pages: 1225 },
+  { title: 'The Catcher in the Rye', author: 'J.D. Salinger', pages: 214 },
+  { title: 'Brave New World', author: 'Aldous Huxley', pages: 311 },
+  { title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 310 },
+  { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', pages: 671 },
+  { title: 'The Odyssey', author: 'Homer', pages: 541 },
+  { title: 'Jane Eyre', author: 'Charlotte Brontë', pages: 500 },
+  { title: 'Fahrenheit 451', author: 'Ray Bradbury', pages: 194 },
+  { title: 'Wuthering Heights', author: 'Emily Brontë', pages: 416 },
+  { title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', pages: 1178 },
+  { title: 'Animal Farm', author: 'George Orwell', pages: 112 },
+  { title: 'Les Misérables', author: 'Victor Hugo', pages: 1463 },
+  { title: 'The Brothers Karamazov', author: 'Fyodor Dostoevsky', pages: 824 },
+  { title: 'The Grapes of Wrath', author: 'John Steinbeck', pages: 464 },
+  { title: 'Don Quixote', author: 'Miguel de Cervantes', pages: 982 },
+];
+
+books.forEach((book) => {
+  addBook(
+    book.title,
+    book.author,
+    book.pages,
+    Math.floor(Math.random() * 2) > 0 ? true : false,
+    Math.floor(Math.random() * 3) + 1,
+  );
+});
+
 const form = document.querySelector('#addBookForm') as HTMLFormElement;
-const dialog = document.querySelector('dialog');
-document.querySelector('#newBook')?.addEventListener('click', () => {
-  dialog?.showModal();
+const showBookDialog = document.querySelector(
+  '#showBookDialog',
+) as HTMLDialogElement;
+
+document.querySelector('#closeDialog')?.addEventListener('click', () => {
+  showBookDialog.close();
+});
+
+showBookDialog.addEventListener('change', (event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.name === 'read') {
+    const selectedValue = target.value === 'true';
+
+    const bookId = showBookDialog.dataset.bookId;
+    const book = libraryMap.get(bookId);
+
+    if (book) {
+      book.read = selectedValue;
+    }
+  }
 });
 
 document.querySelector('#addBookForm')!.addEventListener('submit', (event) => {
@@ -45,48 +95,91 @@ document.querySelector('#addBookForm')!.addEventListener('submit', (event) => {
     shelf: Number(data.get('shelf')),
   };
 
-  const bgColor = getRandomColor();
-  const book = new Book(
-    crypto.randomUUID(),
+  addBook(
     formData.title,
     formData.author,
     formData.pages,
     formData.read,
     formData.shelf,
+  );
+});
+
+function addBook(
+  title: string,
+  author: string,
+  pages: number,
+  read: boolean,
+  shelf: number,
+) {
+  const bgColor = getRandomColor();
+  const book = new Book(
+    crypto.randomUUID(),
+    title,
+    author,
+    pages,
+    read,
+    shelf,
     bgColor,
-    Math.floor(Math.random() * 50 + 50),
+    Math.floor(Math.random() * 50 + 40),
   );
 
   libraryMap.set(book.id, book);
   createBook(book, bgColor);
-
-  requestAnimationFrame(() => {
-    dialog?.close();
-  });
-});
+}
 
 function createBook(book: Book, bgColor: string) {
   const bookshelfRow = document.querySelector(`.shelf-${book.shelf}`);
 
+  const darkerColor = changeColor(bgColor, 20, true);
+  const lighterColor = changeColor(bgColor, 20, false);
+  const height = Math.floor(Math.random() * 20 + 60);
+  const translateZ = Math.floor(Math.random() * 100) - 50;
+
   const bookDiv = document.createElement('div');
-  bookDiv.className = 'book';
-  bookDiv.id = crypto.randomUUID();
+  bookDiv.className = `book`;
+  bookDiv.id = book.id;
   bookDiv.innerHTML = `
-        <div class="title">${book.title}</div>
-        <div class="spine"></div>
-        <div class="left"></div>
-        <div class="right"></div>
-        <div class="top"></div>
-    `;
+  <div class="title">${book.title}</div>
+  <div class="left" style="height:100%; background-color:${darkerColor};"></div>
+  <div class="right"  style="height:100%; background-color:${darkerColor};"></div>
+`;
   bookDiv.style.backgroundColor = book.color;
   bookDiv.style.color = getReadableTextColor(bgColor);
   bookDiv.style.width = book.width + 'px';
-  const height = Math.floor(Math.random() * 20 + 60);
-  //bookDiv.style.height = height + '%';
-  bookDiv.style.height = '150px';
-  const translateZ = Math.floor(Math.random() * 100) - 50;
+  bookDiv.style.setProperty('--outline-color', lighterColor);
+  bookDiv.style.height = height + '%';
+
   bookDiv.dataset.zDepth = translateZ.toString();
   bookDiv.style.transform = `translateZ(${translateZ}px)`;
+
+  bookDiv.addEventListener('click', (e: MouseEvent) => {
+    const target = e.currentTarget as HTMLElement;
+    const id = target.id;
+    const rect = target.getBoundingClientRect();
+    const book: Book = libraryMap.get(id);
+    const title = showBookDialog.querySelector('.title') as HTMLElement;
+    const author = showBookDialog.querySelector('.author') as HTMLElement;
+    const pages = showBookDialog.querySelector('.pages') as HTMLElement;
+
+    showBookDialog.dataset.bookId = id;
+    showBookDialog.style.position = 'absolute';
+    showBookDialog.style.left = `${rect.right}px`;
+    showBookDialog.style.top = `${rect.top}px`;
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages.toString();
+    if (book.read) {
+      document.querySelector<HTMLInputElement>(
+        'input[type="radio"][value="true"]',
+      )!.checked = true;
+    } else {
+      document.querySelector<HTMLInputElement>(
+        'input[type="radio"][value="false"]',
+      )!.checked = true;
+    }
+    showBookDialog.show();
+  });
 
   bookshelfRow?.append(bookDiv);
 }
@@ -98,6 +191,37 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+function changeColor(hex: string, percent: number, dark: boolean) {
+  percent = Math.min(100, Math.max(0, percent));
+
+  hex = hex.replace(/^#/, '');
+
+  // Parse R, G, B
+  let r = parseInt(hex.substring(0, 2), 16);
+  let g = parseInt(hex.substring(2, 4), 16);
+  let b = parseInt(hex.substring(4, 6), 16);
+
+  if (dark) {
+    r = Math.floor(r * (1 - percent / 100));
+    g = Math.floor(g * (1 - percent / 100));
+    b = Math.floor(b * (1 - percent / 100));
+  } else {
+    r = Math.floor(r + (255 - r) * (percent / 100));
+    g = Math.floor(g + (255 - g) * (percent / 100));
+    b = Math.floor(b + (255 - b) * (percent / 100));
+  }
+
+  // Recombine to hex
+  const newHex =
+    '#' +
+    [r, g, b]
+      .map((x) => x.toString(16).padStart(2, '0'))
+      .join('')
+      .toUpperCase();
+
+  return newHex;
 }
 
 function getReadableTextColor(bgColor: string) {
